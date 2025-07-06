13dfad365eb270a38e72fdf40758a62d8bed990b
Ak951717 <mak205709@gmail.com>
2025-07-07 04:17:38 +0500

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/private/',
+      // disallow: '/private/',
     },
     sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
   };
